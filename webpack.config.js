de458efa4dd4aeb84b3cb4ceb2c2e36eacfa2a37
Pasangulapati <Vamsee.Pasangulapati@dcs.IN.gov>
2018-02-20 14:30:41 -0500

    Initial Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,47 @@
 var webpack = require('webpack');
 var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+	'faker',
+	'lodash',
+	'redux',
+	'react-redux',
+	'react-dom',
+	'react-input-range',
+	'redux-form',
+	'redux-thunk',
+	'react',
+];
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+	entry: {
+		bundle: './src/index.js',
+		vendor: VENDOR_LIBS,
+	},
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: '[name].[chunkhash].js',
+	},
+	module: {
+		rules: [
+			{
+				use: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/,
+			},
+			{
+				use: ['style-loader', 'css-loader'],
+				test: /\.css$/,
+			},
+		],
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			names: ['vendor', 'manifest'],
+		}),
+		new htmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
 };
